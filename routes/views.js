7f3d7526f5f24a28cbd9633b84bc2f8424e1073a
Jastria Rahmat <ijash1000@gmail.com>
2019-02-02 21:18:59 +0700

    home page checkpoint

--- routes/views.js
+++ routes/views.js
@@ -2,16 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 let defaultSiteValues = {
+  judul: 'OUTFITKU',
   categories: '/categories',
-  colorTheme: 'teal lighten-1',
+  colorTheme: 'blue-grey darken-3',
   colorThemeText: 'teal',
-  upPageLevel: ''
+  upPageLevel: '',
+
+  link: {
+    imgCDN: 'http://localhost:3000/public/img/',
+    designer: '/designer',
+    about: 'about',
+
+  }
 }
+
+router.get('/', async (req, res) => {
+  const pageVariables = Object.assign(defaultSiteValues, {});
+  res.render('./home/home', pageVariables);
+
+});
 
 router.get('/categories', async (req, res) => {
   const pageVariables = Object.assign(defaultSiteValues, {});
   res.render('./home/categories', pageVariables);
 
 });
+
+
+
 
 module.exports = router;
