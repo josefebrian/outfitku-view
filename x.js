92115c457794819467b853869d1a0b7d74ac6977
Jose Febrian <josefebrian2202@gmail.com>
2019-02-09 20:58:42 +0700

    Auto stash before merge of "master" and "origin/master"

--- x.js
+++ x.js
@@ -1,13 +1,203 @@
+const express = require('express');
+const router = express.Router();
 const axios = require('axios');
+const config = require('config');
+const auth = require('../middleware/auth');
+const authPass = require('../middleware/authPass');
+const apiServer = config.get('APIServer');
 
-const getCat = async function() {
-  await axios({
-      method: 'get',
-      url: 'http://192.168.0.30:3000/api/categories'
-    })
-    .then(function(response) {
-      console.log(response.data)
-    })
+
+let defaultSiteValues = {
+  siteName: 'Outfitku',
+  categories: '/categories',
+  colorTheme: 'blue-grey darken-3',
+  colorThemeText: 'teal',
+  upPageLevel: '',
+
+  link: {
+    imgCDN: config.get('imgCDN'),
+    imgCDNCategories: config.get('imgCDN') + 'categories',
+    designer: '/designers',
+    order: '/order',
+    about: '/about',
+    register: 'register'
+
+  }
 }
 
-getCat()
+
+router.get('/', authPass, auth, async (req, res) => {
+  let pageVariables = Object.assign(defaultSiteValues, { user: req.user });
+
+  res.render('./home/home', pageVariables);
+
+});
+
+router.get('/register', authPass, auth, async (req, res) => {
+
+  if (req.user) return res.send('anda sudah terdaftar');
+  let pageVariables = Object.assign(defaultSiteValues, { user: req.user });
+
+  res.render('./profile/register', pageVariables);
+
+});
+
+router.post('/register', authPass, auth, async (req, res) => {
+  try {
+    console.log(req.body);
+
+    let pageVariables = Object.assign(defaultSiteValues, { user: req.body });
+
+    const newUser = await axios.post(apiServer + '/users/', req.body);
+    pageVariables = Object.assign(defaultSiteValues, req.user);
+    console.log(req.body.name);
+
+    res.render('./profile/register_success', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send(err.response.data)
+  };
+
+
+});
+
+router.get('/designers', authPass, auth, async (req, res) => {
+  try {
+
+    const designers = await axios.get(apiServer + '/designers/');
+    console.log(designers);
+
+    let pageVariables = Object.assign(defaultSiteValues, { user: req.user, designers: designers.data });
+
+    // res.send(designers.data)
+    res.render('./designers/designers', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send('error: ' + err.response.data)
+  };
+});
+
+router.get('/designers/:id', authPass, auth, async (req, res) => {
+  try {
+    const designer = await axios.get(apiServer + '/designers/' + req.params.id);
+    let pageVariables = Object.assign(defaultSiteValues, { user: req.user, designer: designer.data, upPageLevel: '../' });
+
+
+
+    // res.send(templateItem.data)
+    res.render('./designers/designer_page', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send('error: ' + err.response.data)
+  };
+});
+
+router.get('/profile', auth, async (req, res) => {
+  let pageVariables = Object.assign(defaultSiteValues, { user: req.user });
+  // console.log(req.user.name);
+
+  try {
+    const result = await axios.get(apiServer + '/users/' + req.user._id)
+    pageVariables = Object.assign(defaultSiteValues, req.user);
+    // console.log(result);
+
+    res.render('./profile/profile', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send(err.response.data)
+  };
+});
+
+router.get('/profile/create_business', auth, async (req, res) => {
+  try {
+
+    const categories = await axios.get(apiServer + '/categories');
+
+    let pageVariables = Object.assign(defaultSiteValues, { user: req.user, categories: categories.data });
+    // console.log(pageVariables.link.imgCDtegories + categories.data[0].mainImage);NCa
+
+    const result = await axios.get(apiServer + '/users/' + req.user._id)
+    pageVariables = Object.assign(defaultSiteValues, req.user, { upPageLevel: '../' });
+
+    res.render('./profile/create_business', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send(err.response.data)
+  };
+});
+
+router.post('/profile/create_business', auth, async (req, res) => {
+  try {
+
+    const designer = await axios.post(apiServer + '/designers/', req.body);
+
+
+    res.send(req.body)
+    // res.render('./profile/create_business', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send('error: ' + err.response.data)
+  };
+});
+
+router.get('/designers/:id/orders', auth, async (req, res) => {
+  try {
+    const categories = await axios.get(apiServer + '/categories');
+
+    const designer = await axios.get(apiServer + '/designers/' + req.params.id);
+
+    const order = await axios.post(apiServer + '/orders/', { designer: req.params.id });
+
+    let pageVariables = Object.assign(defaultSiteValues, { user: req.user, designer: designer.data, categories: categories.data, order: order.data, upPageLevel: '../../' });
+
+    // res.send(templateItem.data)
+    res.render('./designers/inputOrder', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send('error: ' + err.response.data)
+  };
+});
+
+// pisahin post pake view yang lain
+
+router.post('/designers/:id/orders', auth, async (req, res) => {
+  try {
+    const designer = await axios.get(apiServer + '/designers/' + req.params.id);
+
+    const order = await axios.post(apiServer + '/orders/', { designer: req.params.id });
+
+    let pageVariables = Object.assign(defaultSiteValues, { user: req.user, designer: designer.data, order: order.data, upPageLevel: '../../' });
+
+    res.render('./designers/viewOrder', pageVariables)
+  } catch (err) {
+    res.status(err.response.status).send('error: ' + err.response.data)
+  };
+});
+
+router.get('/designers/:id/orders/:orderId', auth, async (req, res) => {
+  try {
+    const designer = await axios.get(apiServer + '/designers/' + req.params.id);
+    const order = await axios.get(apiServer + '/orders/' + req.params.orderId);
+    let pageVariables = Object.assign(defaultSiteValues, { user: req.user, order: order.data, designer: designer.data, upPageLevel: '../../../' });
+
+    // res.send(templateItem.data)
+    res.render('./designers/inputOrder', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send('error: ' + err.response.data)
+  };
+});
+
+//FOR TESTING PURPOSE
+router.get('/template', async (req, res) => {
+  const pageVariables = Object.assign(defaultSiteValues, {});
+  res.render('./materialize-template/aaa.html', pageVariables);
+
+});
+//TEMPLATE ROUTE
+router.get('/templateRoute', auth, async (req, res) => {
+  try {
+    let pageVariables = Object.assign(defaultSiteValues, { user: req.user });
+    const templateItem = await axios.get(apiServer + '/templateItem/');
+
+
+    res.send(templateItem.data)
+    // res.render('./xxxx/xxxx', pageVariables);
+  } catch (err) {
+    res.status(err.response.status).send('error: ' + err.response.data)
+  };
+});
+
+module.exports = router;
